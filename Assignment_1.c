b4d44d5ecfb8fa4ef797f0fce963c70d2ea4bfd9
SagnikRahman <121563860+SagnikRahman@users.noreply.github.com>
2024-01-13 23:25:00 +0530

    Create Assignment_1.c

---
+++ Assignment_1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+int n;
+printf("Enter the length of array\n");
+scanf("%d",&n);
+int* a=(int*)malloc(n*4);
+
+printf("Enter the elements:\n");
+
+int i=0,x,j;
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+
+printf("Enter sum value(X):\n");
+scanf("%d",&x);
+
+
+i=0;j=n-1;
+while(i<j)
+{
+ if(a[i]+a[j]==x)
+    {
+    printf("%d+%d=%d\n",a[i],a[j],x);
+    i++;
+    j--;
+    }
+ else if(a[i]+a[j]<x)
+    i++;
+  else
+    j--;
+}//while
+  
+}//main
