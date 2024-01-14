7ba5812cdb957fb15dee8d72f5d7535c9893b687
SagnikRahman <121563860+SagnikRahman@users.noreply.github.com>
2024-01-14 18:26:53 +0530

    Update Assignment_1.c

--- Assignment_1.c
+++ Assignment_1.c
@@ -1,33 +1,32 @@
-#include <stdio.h>
-
-int main() {
-    int n, X;
-    printf("Enter the size of the sorted array: ");
-    scanf("%d", &n);
-    int arr[n];
-    printf("Enter the sorted array elements:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-    printf("Enter the target sum (X): ");
-    scanf("%d", &X);
-
-    int left = 0;
-    int right = n - 1;
-
-    while (left < right) {
-        int currentSum = arr[left] + arr[right];
-        if (currentSum == X) {
-            printf("Pair found: %d and %d\n", arr[left], arr[right]);
-            return 0;
-        } else if (currentSum < X) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-
-    printf("Pair not found\n");
-
-    return 0;
+#include <stdio.h> 
+int main() 
+{ 
+    int n; 
+    printf("Enter the length of array:\n"); 
+    scanf("%d",&n); 
+    int arr[n]; 
+    printf("Enter the elements of the sorted array:\n"); 
+    int i,X,left=0,right=n-1,currentSum=0,flag=0; 
+    for(i=0;i<n;i++) 
+    scanf("%d",&arr[i]); 
+    printf("Enter sum value(X):\n"); 
+    scanf("%d",&X); 
+    printf("\nThe possibilities are:\n");
+    while(left<right) 
+    { 
+        currentSum=arr[left]+arr[right];
+        if(currentSum==X) 
+        {
+            flag=1; 
+            printf("%d+%d=%d\n",arr[left],arr[right],X); 
+            left++; 
+            right--; 
+        } 
+        else if(currentSum<X) 
+        left++; 
+        else 
+        right--; 
+    } 
+if(flag==0) 
+printf("No such element pair exists"); 
 }
